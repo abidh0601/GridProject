2440cdf216d41104976cce1e8f204edc43288dfb
abidh0601 <abidh0601@gmail.com>
2020-06-04 18:18:15 -0400

    created addRow and addCol

--- script.js
+++ script.js
@@ -9,3 +9,50 @@ const deleteColumn = () => {
         existingRows.lastChild.remove();
     }
 }
+
+const addRow = function () {
+    let id = document.getElementById('grid');
+    let existingRows = document.getElementsByClassName('individual-row');
+    if (existingRows.length === 0) {
+        let nrow = document.createElement('tr');
+        nrow.className = "individual-row";
+        let ncol = document.createElement('td');
+        ncol.className = "individual-col";
+        nrow.appendChild(ncol);
+        id.appendChild(nrow);
+    }
+    else {
+        let existingCols = document.getElementsByClassName('individual-row')[0].childNodes;
+        let nrow = document.createElement('tr');
+        nrow.className = "individual-row";
+        for (let i = 0; i < existingCols.length; i++) {
+            let ncol = document.createElement('td');
+            ncol.className = "individual-col";
+            nrow.appendChild(ncol);
+        }
+        id.appendChild(nrow);
+    }
+}
+
+const addCol = function () {
+    let id = document.getElementById('grid');
+    let existingRows = document.getElementsByClassName('individual-row');
+
+    if (existingRows.length === 0) {
+        let nrow = document.createElement('tr');
+        nrow.className = "individual-row";
+        let ncol = document.createElement('td');
+        ncol.className = "individual-col";
+
+        nrow.appendChild(ncol);
+        id.appendChild(nrow);
+    }
+
+    else {
+        for (let i = 0; i < existingRows.length; i++) {
+            let ncol = document.createElement('td');
+            ncol.className = "individual-col";
+            existingRows[i].appendChild(ncol);
+        }
+    }
+}
